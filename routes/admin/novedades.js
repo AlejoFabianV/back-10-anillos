f6b0fa1a28ee14f2793599f6b1b113df3d0311d9
Alejo Verhagen <verhagenalejo@gmail.com>
2022-11-07 18:32:30 -0300

    get y post editar, get eliminar

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var novedadesModel = require('./../../models/novedadesModel');
 
 router.get('/', async function(req, res, next) {
-  var novedades = await novedadesModel.getNovedaes();
+  var novedades = await novedadesModel.getNovedades();
   res.render('admin/novedades', {
     layout: 'admin/layout', 
     persona: req.session.nombre,
@@ -36,6 +36,42 @@ router.post('/agregar', async (req, res, next) => {
       error: true, 
       message: 'No se pudo agregar la novedad'
     });
+  }
+});
+
+router.get('/eliminar/:id', async (req, res, next) => {
+  let id = req.params.id;
+  await novedadesModel.deleteNovedadById(id);
+  res.redirect('/admin/novedades')
+});
+
+router.get('/editar/:id', async (req, res, next) => {
+  let id = req.params.id;
+  let novedad = await novedadesModel.getNovedadesById(id);
+  res.render('admin/editar', {
+    layout: 'admin/layout',
+    novedad
+  });
+});
+
+router.post('/editar', async (req, res, next) => {
+  try {
+    let obj = {
+      titulo: req.body.titulo,
+      subtitulo: req.body.subtitulo,
+      descripcion: req.body.descripcion
+    }
+
+    await novedadesModel.editarNovedadesById(obj, req.body.id);
+    res.redirect('/admin/novedades');
+
+  } catch (error) {
+    console.log(error)
+    res.render('admin/editar', {
+      layout: 'admin/layout',
+      error: true,
+      message: 'No se pudo modificar la novedad'
+    })
   }
 });
 
