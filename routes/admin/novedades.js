78ebf62d2743dcd7a35365ff76f97b25307e71a2
Alejo Verhagen <verhagenalejo@gmail.com>
2022-11-07 11:29:07 -0300

    get y post /agregar

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -1,12 +1,42 @@
 var express = require('express');
-const { route } = require('./login');
 var router = express.Router();
+var novedadesModel = require('./../../models/novedadesModel');
 
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
+  var novedades = await novedadesModel.getNovedaes();
   res.render('admin/novedades', {
     layout: 'admin/layout', 
-    persona: req.session.nombre
+    persona: req.session.nombre,
+    novedades,
   });
+});
+
+router.get('/agregar', (req, res, next) => {
+  res.render('admin/agregar', {
+    layout: 'admin/layout'
+  });
+});
+
+router.post('/agregar', async (req, res, next) => {
+  try{
+    if (req.body.titulo != '' && req.body.subtitulo != '' && req.body.descripcion != ''){
+      await novedadesModel.insertNovedad(req.body);
+      res.redirect('/admin/novedades')
+    }else {
+      res.render('admin/agregar' , {
+        layout: 'admin/layout',
+        error: true, 
+        message: 'Todos los campos son requiridos'
+      });
+    }
+  }catch (error) {
+    console.log(error)
+    res.render('admin/agregar' , {
+      layout: 'admin/layout',
+      error: true, 
+      message: 'No se pudo agregar la novedad'
+    });
+  }
 });
 
 module.exports = router; 
