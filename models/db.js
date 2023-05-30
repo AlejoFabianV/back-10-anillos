09031fdab0f8d45d4fedf766295579d57d3641ae
Alejo Verhagen <verhagenalejo@gmail.com>
2023-05-30 15:20:53 -0300

    connection mysql railway

--- models/db.js
+++ models/db.js
@@ -3,10 +3,14 @@ const util = require('util');
 
 const pool = mysql.createPool({
     connectionLimit: 10,
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DB_NAME
+    // host: process.env.MYSQL_HOST,
+    // user: process.env.MYSQL_USER,
+    // password: process.env.MYSQL_PASSWORD,
+    // database: process.env.MYSQL_DB_NAME
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE
 }) 
 
 pool.query = util.promisify(pool.query);
