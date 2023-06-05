10f95ebab7b418ff43681ba970e75c22106339e5
Alejo Verhagen <verhagenalejo@gmail.com>
2023-06-04 22:43:11 -0300

    connection railway

--- models/db.js
+++ models/db.js
@@ -7,7 +7,8 @@ const pool = mysql.createPool({
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DB_NAME,
-    port: process.env.MYSQL_PORT
+    port: process.env.MYSQL_PORT,
+    url: process.env.MYSQL_URL
 }) 
 
 pool.query = util.promisify(pool.query);
