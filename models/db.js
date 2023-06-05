ebc8b5060192a36507b36a7531b60f41a9bd0394
Alejo Verhagen <verhagenalejo@gmail.com>
2023-06-05 14:04:50 -0300

    .

--- models/db.js
+++ models/db.js
@@ -3,11 +3,11 @@ const util = require('util');
 
 const pool = mysql.createPool({
     connectionLimit: 10,
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DB_NAME,
-    port: process.env.MYSQL_PORT
+    host: process.env.MYSQL_HOST || localhost,
+    user: process.env.MYSQL_USER || root,
+    password: process.env.MYSQL_PASSWORD || '',
+    database: process.env.MYSQL_DB_NAME || los-diez-anillos ,
+    port: process.env.MYSQL_PORT || 3306
 }) 
 
 pool.query = util.promisify(pool.query);
