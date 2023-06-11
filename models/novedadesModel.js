5c7bdcd7ec76af10a008800285b83299240068ff
Alejo Verhagen <verhagenalejo@gmail.com>
2023-06-10 22:43:46 -0300

    mejora en las querys

--- models/novedadesModel.js
+++ models/novedadesModel.js
@@ -2,14 +2,14 @@ var pool = require('./db');
 
 async function getNovedades() {
     let query = 'select * from novedades';
-    let rows = await pool.query(query);
+    let rows = pool.query(query);
     return rows;
 }
 
 async function insertNovedad(obj) {
     try{
         var query = 'insert into novedades set  ?';
-        var rows = await pool.query(query, [obj]);
+        var rows = pool.query(query, [obj]);
         return rows;
     } catch (error) {
         console.log(error);
@@ -19,20 +19,20 @@ async function insertNovedad(obj) {
 
 async function deleteNovedadById(id) {
     var query = 'delete from novedades where id = ?';
-    var rows = await pool.query(query, [id]);
+    var rows = pool.query(query, [id]);
     return rows;
 }
 
 async function getNovedadesById(id) {
     var query = 'select * from novedades where id = ?';
-    var rows = await pool.query(query, [id]);
+    var rows = pool.query(query, [id]);
     return rows[0];
 }
 
 async function editarNovedadesById(obj, id) {
     try {
         var query = 'update novedades set ? where id = ?';
-        var rows = await pool.query(query, [obj, id]);
+        var rows = pool.query(query, [obj, id]);
         return rows;
     } catch (error) {
         throw error;
