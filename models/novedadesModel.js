f81e58d50eddd0bf0f506c335db07375e22513ff
Alejo Verhagen <verhagenalejo@gmail.com>
2023-06-12 14:42:02 -0300

    agregue await a las querys

--- models/novedadesModel.js
+++ models/novedadesModel.js
@@ -9,7 +9,7 @@ async function getNovedades() {
 async function insertNovedad(obj) {
     try{
         var query = 'insert into novedades set  ?';
-        var rows = pool.query(query, [obj]);
+        var rows = await pool.query(query, [obj]);
         return rows;
     } catch (error) {
         console.log(error);
@@ -19,20 +19,20 @@ async function insertNovedad(obj) {
 
 async function deleteNovedadById(id) {
     var query = 'delete from novedades where id = ?';
-    var rows = pool.query(query, [id]);
+    var rows = await pool.query(query, [id]);
     return rows;
 }
 
 async function getNovedadesById(id) {
     var query = 'select * from novedades where id = ?';
-    var rows = pool.query(query, [id]);
+    var rows = await pool.query(query, [id]);
     return rows[0];
 }
 
 async function editarNovedadesById(obj, id) {
     try {
         var query = 'update novedades set ? where id = ?';
-        var rows = pool.query(query, [obj, id]);
+        var rows = await pool.query(query, [obj, id]);
         return rows;
     } catch (error) {
         throw error;
