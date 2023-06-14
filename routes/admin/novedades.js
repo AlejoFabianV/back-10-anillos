c6a2f022de44d881c0200c9f8281f664a5ece965
Alejo Verhagen <verhagenalejo@gmail.com>
2023-06-14 15:10:49 -0300

    .

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -20,11 +20,6 @@ router.get('/', async function(req, res, next) {
   novedades = novedades.map(novedad => {
     if (novedad.img_id) {
       const portada = cloudinary.url(novedad.img_id, {
-        transformation: [
-          {width: 90,
-          height:120,
-          crop: 'fill',}
-        ],
         secure: true 
       });
       return {
